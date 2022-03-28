edd1004c5ca5f50bafd649f1a69065598eb56e58
Giancarlo-Cavalieri <giancarlo.0.cavalieri@gmail.com>
2022-03-28 15:12:14 -0300

    Questão 4

--- Q1.sql
+++ Q1.sql
@@ -11,5 +11,35 @@ CREATE OR REPLACE PACKAGE Cliente IS
 	TipoPessoaFisica 	CONSTANT NUMBER := 1;
 	TipoPessoaJuridica 	CONSTANT NUMBER := 2;
 	
+	PROCEDURE AdicionarCliente (Nome VARCHAR2, Nascimento DATE, Faturamento NUMBER, Cpf_Cnpj VARCHAR2, Tipo INTEGER);
+	
+END Cliente;
+/
+
+CREATE OR REPLACE PACKAGE BODY Cliente IS
+	
+	C cli;
+	
+	PROCEDURE AdicionarCliente (Nome 		VARCHAR2, 
+	                            Nascimento 	DATE, 
+								Faturamento NUMBER, 
+								Cpf_Cnpj 	VARCHAR2, 
+								Tipo 		INTEGER) IS
+    
+        retorno_validacao number;
+        
+    BEGIN
+	
+		C.NomeCliente := Nome;
+		C.DataNascimento := Nascimento;
+		C.FaturamentoAnual := Faturamento;
+		C.CpfCnpj := Cpf_Cnpj;
+		C.TipoPessoa := Tipo;
+        
+		INSERT INTO TABELA_CLIENTES VALUES (SEQ_ID_TABELA_CLIENTES.NEXTVAL, C.NomeCliente, C.DataNascimento, C.FaturamentoAnual, C.CpfCnpj, C.TipoPessoa);
+		COMMIT;
+		
+		
+	END;
 END Cliente;
 /
