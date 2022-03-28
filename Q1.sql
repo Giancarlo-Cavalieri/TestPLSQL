3761f228ec92fc0c8d5734582b5d08094bd2c963
Giancarlo-Cavalieri <giancarlo.0.cavalieri@gmail.com>
2022-03-28 15:03:05 -0300

    Questão 2

--- Q1.sql
+++ Q1.sql
@@ -1,5 +1,12 @@
 CREATE OR REPLACE PACKAGE Cliente IS
 
+	TYPE cli IS RECORD (NomeCliente    		VARCHAR2(140),
+						DataNascimento  	DATE,
+						FaturamentoAnual    NUMBER,
+						CpfCnpj     		VARCHAR2(12),
+						TipoPessoa			INTEGER);
+	TYPE tab_cli IS TABLE OF cli
+    INDEX BY BINARY_INTEGER;
 	
 END Cliente;
 /
