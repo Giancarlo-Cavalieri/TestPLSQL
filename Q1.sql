a98f68b887bcc4ecc10a4c33a5e14c215bba5deb
Giancarlo-Cavalieri <giancarlo.0.cavalieri@gmail.com>
2022-03-28 15:16:37 -0300

    Questão 5

--- Q1.sql
+++ Q1.sql
@@ -12,6 +12,7 @@ CREATE OR REPLACE PACKAGE Cliente IS
 	TipoPessoaJuridica 	CONSTANT NUMBER := 2;
 	
 	PROCEDURE AdicionarCliente (Nome VARCHAR2, Nascimento DATE, Faturamento NUMBER, Cpf_Cnpj VARCHAR2, Tipo INTEGER);
+	PROCEDURE ValidarCliente (Retorno OUT INTEGER);
 	
 END Cliente;
 /
@@ -35,11 +36,64 @@ CREATE OR REPLACE PACKAGE BODY Cliente IS
 		C.FaturamentoAnual := Faturamento;
 		C.CpfCnpj := Cpf_Cnpj;
 		C.TipoPessoa := Tipo;
-        
-		INSERT INTO TABELA_CLIENTES VALUES (SEQ_ID_TABELA_CLIENTES.NEXTVAL, C.NomeCliente, C.DataNascimento, C.FaturamentoAnual, C.CpfCnpj, C.TipoPessoa);
-		COMMIT;
 		
+		ValidarCliente(retorno_validacao);
+        
+		IF retorno_validacao > 0 THEN
+		
+			INSERT INTO TABELA_CLIENTES VALUES (SEQ_ID_TABELA_CLIENTES.NEXTVAL, C.NomeCliente, C.DataNascimento, C.FaturamentoAnual, C.CpfCnpj, C.TipoPessoa);
+			COMMIT;
+		
+		END IF;
 		
 	END;
+	
+	PROCEDURE ValidarCliente (Retorno OUT INTEGER) IS
+    
+    PESSOA_FIS_NULO EXCEPTION;
+	PESSOA_JUR_NULO EXCEPTION;
+    
+    BEGIN
+		
+		IF C.TipoPessoa = TipoPessoaFisica THEN
+		
+			Retorno := TipoPessoaFisica;
+            
+			BEGIN
+            
+                IF C.NomeCliente IS NULL OR C.DataNascimento IS NULL OR C.CpfCnpj IS NULL THEN
+				
+                    Retorno := -1;
+                    RAISE PESSOA_FIS_NULO;
+				
+                END IF;
+            
+            EXCEPTION WHEN PESSOA_FIS_NULO THEN
+				
+				DBMS_OUTPUT.PUT_LINE('Dados para pessoa física nulo(s), por favor insira Nome, Data de Nascimento e CPF/CNPJ.');
+            END;
+        END IF;
+        
+        IF C.TipoPessoa = TipoPessoaJuridica THEN
+				
+				Retorno := TipoPessoaJuridica;
+				
+                BEGIN
+                
+                    IF C.NomeCliente IS NULL OR C.CpfCnpj IS NULL OR C.FaturamentoAnual IS NULL THEN
+					
+                        Retorno := -1;
+                        RAISE PESSOA_JUR_NULO;
+				
+                    END IF;
+                
+                EXCEPTION WHEN PESSOA_JUR_NULO THEN
+				
+                    DBMS_OUTPUT.PUT_LINE('Dados para pessoa jurídica nulo(s), por favor insira Nome, CPF/CNPJ e Faturamento Anual.');
+				END;
+                
+        END IF;
+	END;
+	
 END Cliente;
 /
