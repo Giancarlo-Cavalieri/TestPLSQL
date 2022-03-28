8b4ffce6e1ad5494f840d3903141cc0a485c1f20
Giancarlo-Cavalieri <giancarlo.0.cavalieri@gmail.com>
2022-03-28 15:21:22 -0300

    Questão 6

--- Q1.sql
+++ Q1.sql
@@ -13,6 +13,7 @@ CREATE OR REPLACE PACKAGE Cliente IS
 	
 	PROCEDURE AdicionarCliente (Nome VARCHAR2, Nascimento DATE, Faturamento NUMBER, Cpf_Cnpj VARCHAR2, Tipo INTEGER);
 	PROCEDURE ValidarCliente (Retorno OUT INTEGER);
+	PROCEDURE RetornarClientes (Ordenacao INTEGER);
 	
 END Cliente;
 /
@@ -94,6 +95,114 @@ CREATE OR REPLACE PACKAGE BODY Cliente IS
                 
         END IF;
 	END;
+	
+	PROCEDURE RetornarClientes (Ordenacao INTEGER) IS
+
+
+	CURSOR cli_1 IS
+	
+		SELECT ID, NOME, DATA_NASC, FATURAMENTO_ANUAL, CPF_CNPJ, TIPO_PESSOA
+		  FROM TABELA_CLIENTES;
+	
+	CURSOR cli_2 IS
+	
+		SELECT ID, NOME, DATA_NASC, FATURAMENTO_ANUAL, CPF_CNPJ, TIPO_PESSOA
+		  FROM TABELA_CLIENTES
+		 ORDER BY NOME;
+	
+	CURSOR cli_3 IS
+	
+		SELECT ID, NOME, DATA_NASC, FATURAMENTO_ANUAL, CPF_CNPJ, TIPO_PESSOA
+		  FROM TABELA_CLIENTES
+		 ORDER BY FATURAMENTO_ANUAL;
+	
+	CURSOR cli_4 IS
+	
+		SELECT ID, NOME, DATA_NASC, FATURAMENTO_ANUAL, CPF_CNPJ, TIPO_PESSOA
+		  FROM TABELA_CLIENTES
+		 ORDER BY NOME, FATURAMENTO_ANUAL;
+		 
+	c1 cli_1%ROWTYPE;
+	c2 cli_2%ROWTYPE;
+	c3 cli_3%ROWTYPE;
+	c4 cli_4%ROWTYPE;
+	
+	    
+	BEGIN
+	
+		IF Ordenacao = 1 THEN
+		
+			OPEN cli_1;
+
+			LOOP
+				FETCH cli_1 INTO c1;
+					IF cli_1%NOTFOUND THEN
+						EXIT;
+					END IF;
+				
+				dbms_output.put_line(c1.ID || ' - ' || c1.NOME || ' - ' || c1.DATA_NASC || ' - ' || c1.FATURAMENTO_ANUAL || ' - ' || c1.CPF_CNPJ || ' - ' || c1.TIPO_PESSOA);
+				
+			END LOOP;
+			
+			CLOSE cli_1;
+		
+		END IF;
+		
+		IF Ordenacao = 2 THEN
+		
+			OPEN cli_2;
+
+			LOOP
+				FETCH cli_2 INTO c2;
+					IF cli_2%NOTFOUND THEN
+						EXIT;
+					END IF;
+				
+				dbms_output.put_line(c2.ID || ' - ' || c2.NOME || ' - ' || c2.DATA_NASC || ' - ' || c2.FATURAMENTO_ANUAL || ' - ' || c2.CPF_CNPJ || ' - ' || c2.TIPO_PESSOA);
+				
+			END LOOP;
+			
+			CLOSE cli_2;
+		
+		END IF;
+		
+		IF Ordenacao = 3 THEN
+		
+			OPEN cli_3;
+
+			LOOP
+				FETCH cli_3 INTO c3;
+					IF cli_3%NOTFOUND THEN
+						EXIT;
+					END IF;
+				
+				dbms_output.put_line(c3.ID || ' - ' || c3.NOME || ' - ' || c3.DATA_NASC || ' - ' || c3.FATURAMENTO_ANUAL || ' - ' || c3.CPF_CNPJ || ' - ' || c3.TIPO_PESSOA);
+				
+			END LOOP;
+			
+			CLOSE cli_3;
+		
+		END IF;
+		
+		IF Ordenacao = 4 THEN
+		
+			OPEN cli_4;
+
+			LOOP
+				FETCH cli_4 INTO c4;
+					IF cli_4%NOTFOUND THEN
+						EXIT;
+					END IF;
+				
+				dbms_output.put_line(c4.ID || ' - ' || c4.NOME || ' - ' || c4.DATA_NASC || ' - ' || c4.FATURAMENTO_ANUAL || ' - ' || c4.CPF_CNPJ || ' - ' || c4.TIPO_PESSOA);
+				
+			END LOOP;
+			
+			CLOSE cli_4;		
+		
+		END IF;
+	
+	END ;	
 	
 END Cliente;
 /
